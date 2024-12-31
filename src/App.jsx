f5fc2807232a1aecc837ad4bace30a46a8880ed3
Manish Sindur <95417648+manishsindur@users.noreply.github.com>
2024-12-31 13:24:53 +0530

    Add Navbar and MainContent components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+import MainContent from "./components/MainContent";
+
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<Navbar />
+			<MainContent />
+		</div>
+	);
 }
 
 export default App;
